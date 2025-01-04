798549ecf98f9be1062e35f504487bef905c77fa
Mahmoud Mansour <mansour@gmail.com>
2025-01-04 21:05:19 +0200

    Finalize

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,56 +21,59 @@ export default function Navbar() {
   }, []);
 
   return (
-    <>
-      <nav
-        className={`navbar navbar-expand-lg text-white py-4 fixed-top bg-dark ${scrolled ? "scrolled-navbar" : ""}`}
-      >
-        <div className="container">
-          <Link className="navbar-brand text-white fs-2 fw-bold" to="/Framework-Page/">
-            START FRAMEWORK
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link text-white rounded fs-6 fw-bold"
-                  aria-current="page"
-                  to="/Framework-Page/about"
-                >
-                  ABOUT
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link text-white rounded fs-6 fw-bold"
-                  to="/Framework-Page/portfolio"
-                >
-                  PORTFOLIO
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link text-white rounded fs-6 fw-bold"
-                  to="/Framework-Page/contact"
-                >
-                  CONTACT
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+    <nav
+      className={`navbar navbar-expand-lg text-white py-4 fixed-top bg-dark ${
+        scrolled ? "scrolled-navbar" : ""
+      }`}
+    >
+      <div className="container">
+        <Link
+          className="navbar-brand text-white fs-2 fw-bold"
+          to="/Framework-Page/"
+        >
+          START FRAMEWORK
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link text-white rounded fs-6 fw-bold"
+                aria-current="page"
+                to="/Framework-Page/about"
+              >
+                ABOUT
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link text-white rounded fs-6 fw-bold"
+                to="/Framework-Page/portfolio"
+              >
+                PORTFOLIO
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link text-white rounded fs-6 fw-bold"
+                to="/Framework-Page/contact"
+              >
+                CONTACT
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
