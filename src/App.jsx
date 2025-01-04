52424f2f0fc4c8d2d6490559ab92a8f44075b35b
Mahmoud Mansour <mansour@gmail.com>
2025-01-04 18:59:53 +0200

    Finalize the Project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Contact from "./Components/Contact/Contact";
+import Error from "./Components/Error/Error";
+
+const route = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+  { path: "*", element: <Error /> },
+]);
+
+export default function App() {
+  return (
+    <>
+      <RouterProvider router={route} />
+    </>
+  );
+}
