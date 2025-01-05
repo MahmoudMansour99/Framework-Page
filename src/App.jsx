4d01f1d86d778ea094d18b498ba5dd1b48763120
Mahmoud Mansour <mansour@gmail.com>
2025-01-05 19:56:58 +0200

    Finalize

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -7,24 +7,19 @@ import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
 import Error from "./Components/Error/Error";
 
-const route = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "about", element: <About /> },
-        { path: "portfolio", element: <Portfolio /> },
-        { path: "contact", element: <Contact /> },
-      ],
-    },
-    { path: "*", element: <Error /> },
-  ],
+const route = createHashRouter([
   {
-    basename: "/Framework-Page/",
-  }
-);
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+  { path: "*", element: <Error /> },
+]);
 
 export default function App() {
   return (
